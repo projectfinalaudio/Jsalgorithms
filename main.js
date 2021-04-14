558cbb146870cb337b3bacd8b2e0a79de5d271ba
aig <aig@localhost.localdomain>
2021-04-14 12:45:32 +0300

    Static html document with minor Javascript implementation

---
+++ main.js
@@ -0,0 +1,21 @@
+const submitForm = () => {
+    var form = document.querySelector("#form");
+
+    for (i = 0; i < form.length; i++) {
+        form[i].style.display = "none";
+    };
+
+    var submiited = document.getElementById("form");
+    var text = "";
+    var i;
+
+    for (i = 0; i < submitted.length; i++) {
+        text += submiited[i].value + "<br>"
+    };
+};
+
+function headerAnimation() {
+    document.querySelector("#main-heading").addEventListener("click", );
+
+
+};
---
+++ style.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#main-heading").fadeToggle(1000).fadeToggle(1200);
+    $("#content").fadeToggle(1250).fadeToggle(1500);
+    $("#form-container").fadeToggle(1500).fadeToggle(1750);
+});
